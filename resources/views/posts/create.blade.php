5adb79755fa91760bf7731a35e2b7db05fa0f072
NozawaDaishi <nozawa171040@gmail.com>
2021-05-27 16:44:03 +0900

    using layouts & add header footer

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,14 @@
+@extends('layouts.layouts')
+
+@section('title', 'Simple Board')
+
+@section('content')
+
 <form method="POST" action="/posts">
     @csrf
     <input type="text" name="title">
     <input type="text" name="content">
     <input type="submit">
 </form>
+
+@endsection
